49dcbc13275125b0cd0682a206b9b6a3d5edef72
clr1235 <15035291867@163.com>
2023-11-01 10:09:04 +0800

    style(play/src/app): 测试git提交时的代码规范校验

--- play/src/App.vue
+++ play/src/App.vue
@@ -1,67 +1,70 @@
 <template>
-    <div class="play-container">
-        <div class="demo-fit">
-            <div v-for="fit in fits" :key="fit" class="block">
-            <span class="title">{{ fit }}</span>
-            <jl-avatar shape="square" :size="100" :fit="fit" :src="url" />
-        </div>
-        <div class="mb-4">
-            <jl-button>Default</jl-button>
-            <jl-button type="primary">Primary</jl-button>
-            <jl-button type="success">Success</jl-button>
-            <jl-button type="info">Info</jl-button>
-            <jl-button type="warning">Warning</jl-button>
-            <jl-button type="danger">Danger</jl-button>
-        </div>
-    </div>
-    </div>
+	<div class="play-container">
+		<div class="demo-fit">
+			<div v-for="fit in fits" :key="fit" class="block">
+				<span class="title">{{ fit }}</span>
+				<jl-avatar shape="square" :size="100" :fit="fit" :src="url" />
+			</div>
+			<div class="mb-4">
+				<jl-button>Default</jl-button>
+				<jl-button type="primary">Primary</jl-button>
+				<jl-button type="success">Success</jl-button>
+				<jl-button type="info">info 按钮</jl-button>
+				<jl-button type="warning">Warning</jl-button>
+				<jl-button type="danger">Danger</jl-button>
+			</div>
+		</div>
+	</div>
 </template>
-  
+
 <script setup lang="ts">
-    import { reactive, toRefs } from 'vue'
+import { reactive, toRefs } from 'vue'
 
-    const state = reactive({
-    fits: ['fill', 'contain', 'cover', 'none', 'scale-down'],
-    url: 'https://fuss10.elemecdn.com/e/5d/4a731a90594a4af544c0c25941171jpeg.jpeg',
-    })
+const state = reactive({
+	fits: ['fill', 'contain', 'cover', 'none', 'scale-down'],
+	url: 'https://fuss10.elemecdn.com/e/5d/4a731a90594a4af544c0c25941171jpeg.jpeg',
+})
 
-    const { fits, url } = toRefs(state)
+const { fits, url } = toRefs(state)
 </script>
-  
+
 <style lang="scss">
 html,
 body {
-    width: 100vw;
-    height: 100vh;
-    margin: 0;
-    #play {
-      height: 100%;
-      width: 100%;
-      .play-container {
-        height: 100%;
-        width: 100%;
-        display: flex;
-        align-items: center;
-        justify-content: center;
-      }
-    }
+	width: 100vw;
+	height: 100vh;
+	margin: 0;
+
+	#play {
+		width: 100%;
+		height: 100%;
+
+		.play-container {
+			display: flex;
+			align-items: center;
+			justify-content: center;
+			width: 100%;
+			height: 100%;
+		}
+	}
 }
+
 .demo-fit {
-  display: flex;
-  text-align: center;
-  justify-content: space-between;
+	display: flex;
+	justify-content: space-between;
+	text-align: center;
 }
+
 .demo-fit .block {
-  flex: 1;
-  display: flex;
-  flex-direction: column;
-  flex-grow: 0;
+	display: flex;
+	flex: 1;
+	flex-direction: column;
+	flex-grow: 0;
 }
 
 .demo-fit .title {
-  margin-bottom: 10px;
-  font-size: 14px;
-  color: var(--el-text-color-secondary);
+	margin-bottom: 10px;
+	font-size: 14px;
+	color: var(--el-text-color-secondary);
 }
 </style>
-  
